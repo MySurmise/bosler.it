501eccc2b8b2f2b4b7cca4a3825eedfd10bbbc36
mysurmise <marlbos@outlook.de>
2021-09-24 12:14:23 +0200

    added pug as template engine

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,17 @@
-setTimeout(() => {
-  console.log('after ')
-}, 0)
+const express = require("express")
+const pug = require("pug")
+const app = express()
+const port = 3000
+const path = require("path") 
 
-console.log(' before ')
-console.log(' Mitte ')
+app.set('view engine', 'pug')
+app.set('views', __dirname + "/files/Design/views")
+app.use(express.static(path.join(__dirname, 'files')))
+
+app.get('/quote/:quoteid([0-9]+)', (req, res) => {
+  res.render( 'pug/quotetyping')
+})    
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/quote/2`)
+})
