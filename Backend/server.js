f5e962151583a8d04d5d0bb607102743d1240b20
mysurmise <marlbos@outlook.de>
2021-09-27 04:03:12 +0200

    some improvements

--- Backend/server.js
+++ Backend/server.js
@@ -9,22 +9,36 @@ const util = require("util")
 
 
 
+
+const timelog = (objectToLog) => {
+  currentTime = new Date(Date.now())
+  if (typeof (objectToLog) == "string" || typeof (objectToLog) == "undefined" ) {
+      console.log("[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] " + objectToLog)
+
+  } else {
+    console.log("[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] ")
+    console.log(objectToLog)
+  }
+}
+
 app.set('view engine', 'pug')
 app.set('views', __dirname + "/files/Design/views")
 app.use(express.static(path.join(__dirname, 'files')))
-const db = new dbm(path.join(__dirname, 'files/Quotes/DB/Quotes.db'), { verbose: console.log, fileMustExist: true })
+
+const db = new dbm(path.join(__dirname, 'files/Quotes/DB/Quotes.db'), { verbose: timelog, fileMustExist: true })
 var quoteids = new Set()
 
-app.get(/\/(newsingle)?$/, (req, res) => {
+
+app.get(/^\/(newsingle)?$/, (req, res) => {
 
   randomQuote = db.prepare(util.format('SELECT quoteid FROM Quotes ORDER BY RANDOM() LIMIT 1')).get()
   res.redirect('/quote/' + randomQuote.quoteid)
 })
  
 app.get('/quote/:quoteid([0-9]+)', (req, res) => {
-  currentTime = new Date(Date.now())
+  
 
-  console.log("[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] " + "Received GET Request for quote " + req.params.quoteid)
+  timelog("Received GET Request for quote " + req.params.quoteid)
 /*
   quoteData = JSON.parse(fs.readFileSync(path.join(__dirname, 'files/Quotes/quote.json'), 'utf8'))
   
@@ -40,13 +54,14 @@ app.get('/quote/:quoteid([0-9]+)', (req, res) => {
 
   quoteData = db.prepare(util.format('select * from Quotes where quoteid = %d', req.params.quoteid)).get()
   if (typeof (quoteData) === 'undefined') {
-    res.render('pug/quotetyping', {text: "Quote not available."})
+    res.render('pug/quotetyping', {text: "This quote is not available. Come not back later.", authorname: "- The Creator (Not Tyler)", quoteurl : "https://youtube.com/watch?v=dQw4w9WgXcQ"})
     return
   }
-  console.log(quoteData)
+  quoteData.quoteurl = req.url
+  timelog(quoteData)
   res.render('pug/quotetyping', quoteData)
 })    
  
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
+  timelog(`listening at http://localhost:${port}`)
 })
