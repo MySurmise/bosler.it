b34ca99e72f870aaa16e2f116c577bacd9a56524
mysurmise <marlbos@outlook.de>
2021-09-28 22:20:47 +0200

    Made everything client side because Raspi

--- Backend/files/Javascript/addnew.js
+++ Backend/files/Javascript/addnew.js
@@ -1,61 +1,88 @@
-quoteinput = document.getElementById('quoteinput')
-quoteinput.addEventListener('input', check)
-language = document.getElementById('languageinput')
-language.addEventListener('input', checklanguage)
-authorimg = document.getElementById('authorimg')
-authorname = document.getElementById('authornameinput')
-authorname.addEventListener('input', (event) => {
-    if (event.data == " ") {
-        fetch(window.location.origin + '/authorsearch?q=' + authorname.value).then(resp => {
-            return resp.text()
-        })
-        .then(resp => {
-            console.log(resp)
-            if (resp !== "") {
-                authorimg.src = resp
-                console.log(resp)
-            }
+quoteinput = document.getElementById("quoteinput");
+language = document.getElementById("languageinput");
+language.addEventListener("input", checklanguage);
+authorimg = document.getElementById("newauthimg");
+authornameinput = document.getElementById("authornameinput");
 
-        }).catch(err => console.log('ERROR: ', err))
+quoteinput.addEventListener("input", (event) => {
+    quoteinput.style.height = "";
+    quoteinput.style.height = Math.min(quoteinput.scrollHeight, 1000) - 50 + "px";
+    if (event.inputType == "insertFromPaste" || event.data == " ") {
+        url = window.location.origin;
+        console.log(event);
+        console.log(
+            (query = quoteinput.value
+                .replaceAll("\n", " ")
+                .replaceAll("+", "%2B")
+                .replaceAll("&", "%26")
+                .replaceAll("#", "%23"))
+        );
+
+        fetch(url + "/search?query=" + query)
+            .then((response) => {
+                // console.log("Erfolg", response)
+                return response.json();
+            })
+            .then(function (data) {
+                data.forEach((language) => {
+                    console.log(language.lang, language.prob);
+                });
+                language.value = data[0].lang;
+            })
+            .catch((err) => {
+                console.log("Error", err);
+            });
+    }
+});
+
+authornameinput.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        currentauth = authornameinput.value;
+        requeststring =
+            "https://" + language.value + ".wikipedia.org/w/api.php?action=opensearch&search=" +
+            currentauth +
+            "&utf8=&format=json&origin=*";
+        console.log("'" + requeststring + "'");
+        fetch(requeststring)
+            .then((resp) => resp.json())
+            .then((resp) => {
+                console.log(resp);
+                title = resp[1][0];
+                link = resp[3][0];
+                document.getElementById("newauthlink").href = link;
+                document.getElementById("authorurlinput").value = link;
+                fetch("https://" +
+                    language.value +
+                    ".wikipedia.org/w/api.php?action=query&prop=pageimages&titles=" +
+                    title +
+                    "&format=json&origin=*").then((resp) => resp.json())
+                    .then((resp) => {
+                        console.log(
+                            img =
+                            "https://commons.wikimedia.org/wiki/Special:FilePath/" +
+                            Object.values(resp.query.pages)[0].pageimage)
+                        document.getElementById("authorimginput").value = img;
+                        authorimg.src = img;
+                    });
+               
+
+            });
+        
+    
     }
 })
 
-window.onload = function() {
-    quoteinput.value = ""
-}
+
+window.onload = function () {
+    quoteinput.value = "";
+};
 
 function checklanguage() {
-    if (language.value.length > 2) {
-        language.style.color = "red"
+    if (language.value.length > 2 || language.value.length == 1) {
+        language.style.color = "red";
     } else {
-        language.style.color = "black"
+        language.style.color = "black";
     }
 }
 
-function checkauthor(event) {
-    
-}
-
-function check() {
-
-    quoteinput.style.height = ""
-    quoteinput.style.height = Math.min(quoteinput.scrollHeight, 1000) - 50 + "px"
-    url = window.location.origin
-    console.log(url)
-    console.log(query = quoteinput.value.replaceAll("\n", " ").replaceAll("+", "%2B").replaceAll("&", "%26").replaceAll("#", "%23"))
-    if (query.length > 10) {
-        fetch(url + '/search?query=' + query)
-            .then(response => {
-                // console.log("Erfolg", response)
-                return response.json();
-            }).then(function (data) {
-                data.forEach((language) => {
-                    console.log(language.lang, language.prob)
-                })
-                language.value = data[0].lang
-            })
-            .catch(err => {
-                console.log("Error", err)
-            })
-    }
-}
+function checkauthor(event) { }
--- Backend/server.js
+++ Backend/server.js
@@ -1,116 +1,160 @@
-const express = require("express")
-const pug = require("pug")
-const app = express()
-const port = 5000
-const path = require("path")
-const fs = require("fs")
-const dbm = require("better-sqlite3")
-const util = require("util")
-const langdetect = require("langdetect")
-const wiki = require("wikijs").default
-const fetch = require("node-fetch")
+const express = require("express");
+const pug = require("pug");
+const app = express();
+const port = 5000;
+const path = require("path");
+const fs = require("fs");
+const dbm = require("better-sqlite3");
+const util = require("util");
+const langdetect = require("langdetect");
+const wiki = require("wikijs").default;
+const fetch = require("node-fetch");
 
 const timelog = (objectToLog) => {
-  currentTime = new Date(Date.now())
-  if (typeof (objectToLog) == "string" || typeof (objectToLog) == "undefined" ) {
-      console.log("[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] " + objectToLog)
-
+  currentTime = new Date(Date.now());
+  if (typeof objectToLog == "string" || typeof objectToLog == "undefined") {
+    console.log(
+      "[" +
+        ("" + currentTime).split(" ").slice(1, 5).join(" ") +
+        "] " +
+        objectToLog
+    );
   } else {
-    console.log("[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] ")
-    console.log(objectToLog)
+    console.log(
+      "[" + ("" + currentTime).split(" ").slice(1, 5).join(" ") + "] "
+    );
+    console.log(objectToLog);
   }
-}
+};
 
-app.set('view engine', 'pug')
-app.set('views', __dirname + "/files/Design/views")
-app.use(express.static(path.join(__dirname, 'files')))
+app.set("view engine", "pug");
+app.set("views", __dirname + "/files/Design/views");
+app.use(express.static(path.join(__dirname, "files")));
 
-const db = new dbm(path.join(__dirname, 'files/Quotes/DB/Quotes.db'), { verbose: timelog, fileMustExist: true })
-var quoteids = new Set()
+const db = new dbm(path.join(__dirname, "files/Quotes/DB/Quotes.db"), {
+  verbose: timelog,
+  fileMustExist: true,
+});
+var quoteids = new Set();
+
+app.get(["/", "/newsingle"], (req, res) => {
+  randomQuote = db
+    .prepare("SELECT quoteid FROM Quotes ORDER BY RANDOM() LIMIT 1")
+    .get();
+  res.redirect("/quote/" + randomQuote.quoteid);
+});
+
+app.get("/addnew", (req, res) => {
+  console.log("Addnew");
+  authors = db.prepare("select distinct authorname from quotes").all();
+  authorArray = [];
+  authors.forEach((entry) => {
+    authorArray.push(entry.authorname);
+  });
+  console.log(authorArray);
+  res.render("pug/addnew.pug", { authorArray });
+});
+
+app.get("/search", (req, res) => {
+  console.log(req.query.query);
+  res.json(langdetect.detect(req.query.query));
+});
 
 
-app.get(['/', '/newsingle'], (req, res) => {
-
-  randomQuote = db.prepare('SELECT quoteid FROM Quotes ORDER BY RANDOM() LIMIT 1').get()
-  res.redirect('/quote/' + randomQuote.quoteid)
-})
-
-
-app.get('/addnew', (req, res) => {
-  console.log('Addnew')
-    authors = db.prepare('select distinct authorname from quotes').all()
-    authorArray = []
-    authors.forEach((entry) => {
-    authorArray.push(entry.authorname)
-  })
-  console.log(authorArray)
-  res.render('pug/addnew.pug', {authorArray})
-})
-
-app.get('/search', (req, res) => {
-  console.log(req.query.query)
-  res.json(langdetect.detect(req.query.query))
-})
-
-app.get('/authorsearch', (req, res) => {
-  currentauth = req.query.q
-  console.log('"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + currentauth + '&utf8=&format=json"')
-  fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + currentauth + '&utf8=&format=json')
-    .then(resp => resp.json()).then(resp => {
-    author = Object.values(resp.query.search)[0].title
-    if (typeof author !== "undefined") {
-      return author
-    } else { throw new Error('Nothing found!') }
-  })
-    .then(author => {
-      console.log('"https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=' + author + '&format=json"')
-      return fetch('https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=' + author + '&format=json')
-}).then(resp => resp.json())
-    .then(resp => {
-      console.log(resp = Object.values(resp.query.pages)[0].pageimage)
-      return resp
-    }).then(resp => {
-      if(typeof(resp) !== "undefined") {
-        res.send('https://commons.wikimedia.org/wiki/Special:FilePath/' + resp)
+/* Authorsearch. Abandoned because also possible in browser and my Server is literally a Raspi.
+app.get("/authorsearch", (req, res) => {
+  currentauth = req.query.q;
+  console.log(
+    '"https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' +
+      currentauth +
+      '&utf8=&format=json"'
+  );
+  fetch(
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" +
+      currentauth +
+      "&utf8=&format=json"
+  )
+    .then((resp) => resp.json())
+    .then((resp) => {
+      author = Object.values(resp.query.search)[0].title;
+      if (typeof author !== "undefined") {
+        return author;
       } else {
-        res.send(resp)
+        throw new Error("Nothing found!");
       }
-    }).catch(err => console.log(err))
+    })
+    .then((author) => {
+      authorname = console.log(
+        '"https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=' +
+          author +
+          '&format=json"'
+      );
+      return fetch(
+        "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=" +
+          author +
+          "&format=json"
+      );
+    })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log((resp = Object.values(resp.query.pages)[0].pageimage));
+      if (typeof resp !== "undefined") {
+        fetch(
+          "https://en.wikipedia.org/w/api.php?action=query&prop=info&titles=" +
+            author +
+            "&inprop=url&format=json"
+        )
+          .then((resp) => resp.json())
+          .then((erg) => {
+            author = Object.values(erg.query.pages)[0]
+            response = {
+              name: author.title,
+              src: "https://commons.wikimedia.org/wiki/Special:FilePath/" + resp,
+              link: author.fullurl
+            };
+            res.json(response);
+          })
+      } else {
+        console.log('Debug 1')
+        res.json();
+      }
+    })
+    .catch((err) => console.log(err));
+});
+*/
 
+app.get("/quote/:quoteid([0-9]+)", (req, res) => {
+  timelog("Received GET Request for quote " + req.params.quoteid);
+  /*
+    quoteData = JSON.parse(fs.readFileSync(path.join(__dirname, 'files/Quotes/quote.json'), 'utf8'))
+    
+    authorurl: quoteData.authorurl,
+      authorimg: quoteData.authorimg,
+      originurl: quoteData.originurl,
+      originimg: quoteData.originimg,
+      text: quoteData.text,
+      authorname: quoteData.authorname,
+      quoteurl: "/quote/" + req.params.quoteid
+      */
 
-})
-
-
-
-app.get('/quote/:quoteid([0-9]+)', (req, res) => {
-  
-
-  
-
-  timelog("Received GET Request for quote " + req.params.quoteid)
-/*
-  quoteData = JSON.parse(fs.readFileSync(path.join(__dirname, 'files/Quotes/quote.json'), 'utf8'))
-  
-  authorurl: quoteData.authorurl,
-    authorimg: quoteData.authorimg,
-    originurl: quoteData.originurl,
-    originimg: quoteData.originimg,
-    text: quoteData.text,
-    authorname: quoteData.authorname,
-    quoteurl: "/quote/" + req.params.quoteid
-    */
-  
-
-  quoteData = db.prepare(util.format('select * from Quotes where quoteid = %d', req.params.quoteid)).get()
-  if (typeof (quoteData) === 'undefined') {
-    res.render('pug/quotetyping', {text: "This quote is not available. Come not back later.", authorname: "- The Creator (Not Tyler)", quoteurl : "https://youtube.com/watch?v=dQw4w9WgXcQ"})
-    return
+  quoteData = db
+    .prepare(
+      util.format("select * from Quotes where quoteid = %d", req.params.quoteid)
+    )
+    .get();
+  if (typeof quoteData === "undefined") {
+    res.render("pug/quotetyping", {
+      text: "This quote is not available. Come not back later.",
+      authorname: "- The Creator (Not Tyler)",
+      quoteurl: "https://youtube.com/watch?v=dQw4w9WgXcQ",
+    });
+    return;
   }
-  quoteData.quoteurl = req.url
-  timelog(quoteData)
-  res.render('pug/quotetyping', quoteData)
-})    
- 
+  quoteData.quoteurl = req.url;
+  timelog(quoteData);
+  res.render("pug/quotetyping", quoteData);
+});
+
 app.listen(port, () => {
-  timelog(`listening at http://localhost:${port}`)
-})
+  timelog(`listening at http://localhost:${port}`);
+});
